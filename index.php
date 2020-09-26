94c64ff947570a5244199dc4206600279ca08cb2
Prahlad Nayak <pnayak1501@gmail.com>
2020-09-26 11:51:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+
+    <style>
+        body{
+            background: url("images/back2.jpg") no-repeat center center fixed;
+            background-size: cover;
+        }
+/*
+        body{
+            background: linear-gradient(to right,#8eff00,#2b8957);
+        }
+*/
+    </style>
+    <title>Index</title>
+</head>
+<body>
+    
+    <p style="color:#232a34; font-size:5rem; text-align:center; margin-top:15%; font-weight:bold; font-family: 'Roboto', sans-serif;
+" class="display-1">Get Internships Now with <br> Svadhyaya!</p>
+    
+    <div style="text-align:center;">
+        <a href="all_posts.php"><button class="btn btn-success"> Apply Now</button></a>
+        <a href="company_login.php"><button class="btn btn-danger"> Recruit Now</button></a>
+    </div>
+    
+</body>
+</html>
